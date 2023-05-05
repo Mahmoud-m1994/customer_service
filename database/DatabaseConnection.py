f6e45b8110ad2e8aefe25b3485fac74caf9f94b6
Mahmoud <mahmoud.ibrahim.mohammad@rikstv.no>
2023-05-05 14:11:03 +0200

    setup structure for project and created a db connector file

---
+++ database/DatabaseConnection.py
@@ -0,0 +1,21 @@
+import pyodbc
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+
+def create_db_connector():
+    server = os.getenv('SERVER')
+    database = os.getenv('DATABASE')
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
+    driver = os.getenv('DRIVER')
+    connection_string = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+    try:
+        conn = pyodbc.connect(connection_string)
+        print("Connection successful!", conn)
+        return conn
+    except Exception as e:
+        print("Error connecting to database:", e)
+        return None
