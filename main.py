9f97c3dbdbc73f00adda8a3ef432982495175af1
Mahmoud <mahmoud.ibrahim.mohammad@rikstv.no>
2023-05-21 09:43:25 +0200

    added issue_api to main.py

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from flask import Flask
 from api.SellerApi import seller_api
 from api.ProductApi import product_api
 from api.OrderApi import order_api
+from api.IssueApi import issues_api
 
 app = Flask(__name__)
 
 app.register_blueprint(seller_api)
 app.register_blueprint(product_api)
 app.register_blueprint(order_api)
+app.register_blueprint(issues_api)
 
 if __name__ == '__main__':
     print("Hei from customer service")
